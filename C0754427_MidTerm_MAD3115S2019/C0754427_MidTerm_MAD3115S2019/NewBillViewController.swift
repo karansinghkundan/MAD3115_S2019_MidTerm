1fb62877119cf9d9c140bcc7bc554ab5ea762c6d
karan <karansinghkundan@gmail.com>
2019-07-12 19:30:36 -0400

    Date Picker added

--- C0754427_MidTerm_MAD3115S2019/C0754427_MidTerm_MAD3115S2019/NewBillViewController.swift
+++ C0754427_MidTerm_MAD3115S2019/C0754427_MidTerm_MAD3115S2019/NewBillViewController.swift
@@ -19,5 +19,35 @@ class NewBillViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func showDatePicker(){
+        //Formate Date
+        datePicker.datePickerMode = .date
+        
+        //ToolBar
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(donedatePicker));
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDatePicker));
+        
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        
+        txtDatePicker.inputAccessoryView = toolbar
+        txtDatePicker.inputView = datePicker
+        
+    }
+    
+    @objc func donedatePicker(){
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        txtDatePicker.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func cancelDatePicker(){
+        self.view.endEditing(true)
+    }
+}
 
 }
